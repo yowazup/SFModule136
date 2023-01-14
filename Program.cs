c92b0a158933afe0bc3c5ef0567eab9321ba9c55
Nikita <nikit.zakharov@gmail.com>
2023-01-14 03:33:37 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Tracing;
+using System.Globalization;
+using System.Linq;
+using System.Runtime;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Xml.Linq;
+
+
+namespace SFModule136
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "D:/Text1.txt";
+
+            if (File.Exists(filePath)) // Проверим, что файл существует
+            {
+                // Читаем содержимое файла в строку текста, убираем пунктуацию и приводим все буквы к нижнему регистру
+                string text = File.ReadAllText(filePath);
+                var noPunctuationText = new string(text.ToLower().Where(c => !char.IsPunctuation(c)).ToArray());
+
+                // Сохраняем символы-разделители в массив
+                char[] delimiters = new char[] { ' ', '\r', '\n' };
+
+                // разбиваем строку текста, используя ранее перечисленные символы-разделители
+                var words = noPunctuationText.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+                // сложим элементы в обычный List
+                var wordsList = new List<string>();
+
+                foreach (string word in words)
+                    wordsList.Add(word);
+
+                // сложим элементы в List со ссылками
+                var wordsLinkedList = new LinkedList<string>();
+
+                foreach (string word in words)
+                    wordsLinkedList.AddLast(word);
+
+                // выведем количество элементов в каждой коллекции
+                Console.WriteLine("Размер коллекции List: {0} слов.", wordsList.Count);
+                Console.WriteLine("Размер коллекции LinkedList: {0} слов.", wordsLinkedList.Count);
+                Console.WriteLine();
+
+                // добавим элемент внутрь коллекции List и выведем время добавления к консоль
+                var stopWatch1 = Stopwatch.StartNew();
+                wordsList.Insert(2, "тест");
+                Console.WriteLine("Время добавления элемента в List заняло {0} мс.", stopWatch1.Elapsed.TotalMilliseconds);
+
+                // добавим элемент внутрь коллекции LinkedList и выведем время добавления к консоль
+                var stopWatch2 = Stopwatch.StartNew();
+                wordsLinkedList.AddAfter(wordsLinkedList.First, "тест");
+                Console.WriteLine("Время добавления элемента в LinkedList заняло {0} мс.", stopWatch2.Elapsed.TotalMilliseconds);
+
+                // выведем количество элементов в коллекции после добавления элемента
+                Console.WriteLine();
+                Console.WriteLine("Размер обновленной коллекции List: {0} слов.", wordsList.Count);
+                Console.WriteLine("Размер обновленной коллекции LinkedList: {0} слов.", wordsLinkedList.Count);
+
+                // выведем топ-10 уникальных слов
+                Console.WriteLine();
+                Console.WriteLine("Топ-10 самых повторяющихся слов:");
+                Top10UniqueWords(wordsList);
+            }
+            else
+            {
+                Console.WriteLine("Передан некорректный путь к файлу для работы.");
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+        static void Top10UniqueWords(List<string> wordsList)
+        {
+            var uniqueWords = new HashSet<string>(wordsList);
+            var counter = new Dictionary<string, int>();
+
+            foreach (string word in uniqueWords)
+                counter.Add(word, word.Count());
+
+            var sortedCounter = from item in counter orderby item.Value descending select item;
+
+            var top10 = sortedCounter.Take(10);
+
+            foreach (KeyValuePair<string, int> x in top10)
+                Console.WriteLine($"{x.Key} = {x.Value}");
+        }
+    }
+}
